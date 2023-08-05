c9c801149c3d53c3df2b1769885b2f286df91bd1
SabigGasim <sabgasim@gmail.com>
2023-08-05 20:15:52 +0300

    added file I/O functionality for the native app

---
+++ Social_Media_App_v2/NativeApp/Infrastructure/Data/FileManagers/MauiFileManager.cs
@@ -0,0 +1,38 @@
+﻿using Domain.Common;
+using NativeApp.Interfaces;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace NativeApp.Infrastructure.Data.FileManagers;
+public class MauiFileManager : IFileManager
+{
+    public async Task<Result<string>> ReadFile(string path)
+    {
+        try
+        {
+            using var fileStream = await FileSystem.Current.OpenAppPackageFileAsync(path);
+            using var reader = new StreamReader(fileStream);
+            var text = reader.ReadToEnd();
+            return Results.Success(text);
+        }
+        catch (Exception ex)
+        {
+            return Results.Fail<string>(ex);
+        }
+    }
+
+    public async Task<Result> WriteToFile(string path, string text)
+    {
+        try
+        {
+            using var fileStream = await FileSystem.Current.OpenAppPackageFileAsync(path);
+            using var writer = new StreamWriter(fileStream);
+            writer.Write(text);
+
+            return Results.Success();
+        }
+        catch(Exception ex)
+        {
+            return Results.Fail(ex);
+        }
+    }
+}
