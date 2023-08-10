fe377fb60956a28da0fc97236d8c15b135d82aa4
Sabig Gasim Bakry <sabgasim@gmail.com>
2023-08-10 10:03:03 +0300

    added a handler for user lookup

---
+++ Social_Media_App_v2/NativeApp/Interfaces/ISearchHandler.cs
@@ -0,0 +1,10 @@
+﻿using System.Timers;
+
+namespace NativeApp.Interfaces;
+
+public interface ILookupHandler
+{
+    void ResetAndStartSearchTimer(string query);
+    Func<string, Task> Handle { get; set; }
+    double Interval { get; set; }
+}
---
+++ Social_Media_App_v2/NativeApp/Services/LookupHandler.cs
@@ -0,0 +1,45 @@
+﻿using NativeApp.Interfaces;
+using System.Timers;
+
+namespace NativeApp.Services;
+
+public class LookupHandler : ILookupHandler
+{
+    private readonly System.Timers.Timer _timer;
+    private string? _newQuery;
+    private string? _oldQuery;
+
+    public LookupHandler()
+    {
+        _timer = new System.Timers.Timer();
+        _timer.AutoReset = false;
+        _timer.Elapsed += TimerElapsedEventHandler;
+    }
+
+    public double Interval
+    {
+        get => _timer.Interval;
+        set => _timer.Interval = value;
+    }
+
+    public Func<string, Task> Handle { get; set; } = default!;
+
+    public void ResetAndStartSearchTimer(string query)
+    {
+        _timer.Stop();
+        _newQuery = query;
+        _timer.Start();
+    }
+
+    private async void TimerElapsedEventHandler(object? sender, ElapsedEventArgs e)
+    {
+        if(_oldQuery == _newQuery)
+        {
+            return;
+        }
+
+        _oldQuery = _newQuery;
+
+        await Handle(_newQuery!);
+    }
+}
