bb5ec387993039e3fc0c2926eb3c44582fe7fac8
Sabig Gasim Bakry <sabgasim@gmail.com>
2023-08-10 03:27:32 +0300

    added the FollowRequests view model with fake data

---
+++ Social_Media_App_v2/NativeApp/MVVM/ViewModels/FollowRequestViewModel.cs
@@ -0,0 +1,36 @@
+﻿using Infrastructure.Interfaces;
+using NativeApp.Helpers;
+using NativeApp.MVVM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NativeApp.MVVM.ViewModels;
+
+public class FollowRequestViewModel : ViewModelBase
+{
+    private ObservableList<FollowRequestModel>? _requests = new();
+    private readonly IFollowRequestsRepository _repository;
+
+    public FollowRequestViewModel(IFollowRequestsRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task UpdateFollowReuqests(Guid? lastSeenReuqest, int numberOfReuqests)
+    {
+        var result = (await _repository.GetFollowRequests(null, lastSeenReuqest, numberOfReuqests));
+        if (result.Success)
+        {
+            Requests?.AddRange(result.Value!.Map());
+        }
+    }
+
+    public ObservableList<FollowRequestModel>? Requests 
+    {
+        get => _requests;
+        set => TrySetValue(ref _requests, value);
+    }
+}
