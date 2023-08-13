1477a59d6ce4e7fc71330056fe1f029ce15ff735
Sabig Gasim Bakry <sabgasim@gmail.com>
2023-08-13 15:49:56 +0300

    added ProfileViewModel.cs

---
+++ Social_Media_App_v2/NativeApp/MVVM/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,74 @@
+﻿using Infrastructure.Interfaces;
+using NativeApp.Helpers;
+using System.Windows.Input;
+
+namespace NativeApp.MVVM.ViewModels;
+
+public class ProfileViewModel : ViewModelBase
+{
+    private RangeObservableCollection<PostViewModel>? _postViewModels = new();
+    private UserViewModel? _userViewModel;
+    private ICommand? _followButtonCommand;
+    private readonly IPostRepository _repository;
+    private readonly IServiceProvider _serviceProvider;
+
+    public ProfileViewModel(
+        IPostRepository repository,
+        IServiceProvider serviceProvider,
+        UserViewModel userViewModel)
+    {
+        _repository = repository;
+        _serviceProvider = serviceProvider;
+        _userViewModel = userViewModel;
+
+        InitializeCommands();
+    }
+
+    private void InitializeCommands()
+    {
+        _followButtonCommand = new Command(() =>
+        {
+            UserViewModel!.User!.IsUserBeingFollowed = !UserViewModel.User.IsUserBeingFollowed;
+            OnPropertyChanged(nameof(UserViewModel));
+        });
+    }
+
+    public async Task UpdatePosts(Guid? lastSeenPost, int numberOfPosts)
+    {
+        var result = await _repository.GetUserPosts(UserViewModel!.User!.Id, lastSeenPost, numberOfPosts);
+        if (!result.Success)
+        {
+            return;
+        }
+
+        var posts = result.Value!.Map().ToList();
+        if (posts.Count <= 0 || posts.Count >= 350)
+        {
+            return;
+        }
+
+        var postViewModels = posts.Select(post =>
+        {
+            var postViewModel = _serviceProvider.GetRequiredService<PostViewModel>();
+            postViewModel!.Post = post;
+            return postViewModel;
+        });
+
+
+        PostViewModels!.AddRange(postViewModels.Reverse());
+    }
+
+    public UserViewModel? UserViewModel 
+    { 
+        get => _userViewModel; 
+        set => TrySetValue(ref _userViewModel, value); 
+    }
+
+    public RangeObservableCollection<PostViewModel>? PostViewModels
+    {
+        get => _postViewModels;
+        set => TrySetValue(ref _postViewModels, value);
+    }
+
+    public ICommand? FollowButtonCommand => _followButtonCommand;
+}
