12bf3fb8631e822495108c83dcb900dd7df9fa51
Sabig Gasim Bakry <sabgasim@gmail.com>
2023-08-15 14:59:55 +0300

    added base,user,account alert view models

---
+++ Social_Media_App_v2/NativeApp/MVVM/ViewModels/AccountAlertViewModel.cs
@@ -0,0 +1,18 @@
+﻿using NativeApp.MVVM.Models;
+using System.Windows.Input;
+
+namespace NativeApp.MVVM.ViewModels;
+
+public class AccountAlertViewModel : AlertViewModelBase<AccountAlertModel>
+{
+    private AccountAlertModel? _accountAlertModel;
+    private ICommand? _alertClickedCommand;
+
+    public override AccountAlertModel? Alert 
+    { 
+        get => _accountAlertModel;
+        set => TrySetValue(ref _accountAlertModel, value); 
+    }
+
+    public override ICommand? AlertClickedCommand => _alertClickedCommand;
+}
---
+++ Social_Media_App_v2/NativeApp/MVVM/ViewModels/AlertsViewModel.cs
@@ -0,0 +1,53 @@
+﻿using Infrastructure.Interfaces;
+using NativeApp.Helpers;
+using NativeApp.MVVM.Models;
+
+namespace NativeApp.MVVM.ViewModels;
+
+public class AlertsViewModel : ViewModelBase
+{
+    private RangeObservableCollection<AlertViewModelBase>? _alerts = new();
+    private readonly IAlertsRepository _repository;
+    private readonly IServiceProvider _serviceProvider;
+
+    public AlertsViewModel(
+        IAlertsRepository repository,
+        IServiceProvider serviceProvider)
+    {
+        _repository = repository;
+        _serviceProvider = serviceProvider;
+    }
+
+    public async Task UpdateAlerts(Guid? lastSeenAlertId, int numberOfAlerts)
+    {
+        var result = await _repository.GetAlerts(Guid.NewGuid(), lastSeenAlertId, numberOfAlerts);
+        
+        if (result.Success)
+        {
+            var alerts = result.Value!.Map().Select(GetAlertViewModel).ToList();
+            Alerts!.AddRange(alerts);
+        }
+    }
+
+    private AlertViewModelBase GetAlertViewModel(AlertModelBase alert)
+    {
+        if (alert is UserAlertModel userAlert)
+        {
+            var userAlertViewModel = _serviceProvider.GetRequiredService<UserAlertViewModel>();
+            userAlertViewModel.Alert = userAlert;
+            return userAlertViewModel;
+        }
+
+        var accountAlert = (AccountAlertModel)alert;
+        var accountAlertViewModel = _serviceProvider.GetRequiredService<AccountAlertViewModel>();
+        accountAlertViewModel.Alert = accountAlert;
+
+        return accountAlertViewModel;
+    }
+
+    public RangeObservableCollection<AlertViewModelBase>? Alerts
+    {
+        get => _alerts;
+        set => TrySetValue(ref _alerts, value);
+    }
+}
