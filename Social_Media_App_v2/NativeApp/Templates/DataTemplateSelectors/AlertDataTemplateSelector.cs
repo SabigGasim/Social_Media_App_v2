d9271cfdc4b8eda8b425e7d918907f90e6a0117a
Sabig Gasim Bakry <sabgasim@gmail.com>
2023-08-15 15:08:57 +0300

    added AlertDataTemplateSelector'

---
+++ Social_Media_App_v2/NativeApp/Templates/DataTemplateSelectors/AlertDataTemplateSelector.cs
@@ -0,0 +1,16 @@
+﻿using NativeApp.MVVM.ViewModels;
+
+namespace NativeApp.Templates.DataTemplateSelectors;
+
+public class AlertDataTemplateSelector : DataTemplateSelector
+{
+    public DataTemplate AccountAlertDataTemplate { get; set; } = default!;
+    public DataTemplate UserAlertDataTemplate { get; set; } = default!;
+
+    protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+    {
+        return item is UserAlertViewModel
+            ? UserAlertDataTemplate
+            : AccountAlertDataTemplate;
+    }
+}
