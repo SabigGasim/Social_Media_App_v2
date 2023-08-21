85799df176c6d349eea23e99fc0f9a4333e90764
Sabig Gasim Bakry <sabgasim@gmail.com>
2023-08-21 17:26:03 +0300

    refactor route rigester code to use view's FullName
    Routes.RigesterRoutes method now rigesters the view name with the namespace to allow better project artchitecture

--- Social_Media_App_v2/NativeApp/Constants/Routes.cs
+++ Social_Media_App_v2/NativeApp/Constants/Routes.cs
@@ -4,10 +4,10 @@ namespace NativeApp.Constants;
 
 internal class Routes
 {
-    internal const string CommentsPage = nameof(MVVM.Views.CommentsPage);
-    internal const string RepliesPage = nameof(MVVM.Views.RepliesPage);
-    internal const string MediaViewer = nameof(MVVM.Views.PostMediaViewer);
-    internal const string ProfilePage = nameof(MVVM.Views.ProfilePage);
+    internal readonly string CommentsPage = typeof(MVVM.Views.CommentsPage).FullName!;
+    internal readonly string RepliesPage = typeof(MVVM.Views.RepliesPage).FullName!;
+    internal readonly string MediaViewer = typeof(MVVM.Views.PostMediaViewer).FullName!;
+    internal readonly string ProfilePage = typeof(MVVM.Views.ProfilePage).FullName!;
 
     internal static void RegisterRoutes()
     {
@@ -15,10 +15,12 @@ internal class Routes
 
         foreach (var field in fields)
         {
-            if (field.IsLiteral && !field.IsInitOnly)
+            if (field.IsLiteral)
             {
-                var pageName = (string)field.GetRawConstantValue()!;
-                var type = Type.GetType($"NativeApp.MVVM.Views.{pageName}");
+                var pageName = field.Name;
+                var pageFullName = (string)field.GetRawConstantValue()!;
+                var type = Type.GetType(pageFullName);
+
                 Routing.RegisterRoute(pageName, type);
             }
         }
