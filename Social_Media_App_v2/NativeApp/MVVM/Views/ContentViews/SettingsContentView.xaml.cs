4e55fc42e4136b2517c7401b798a1973b28357d7
Sabig Gasim Bakry <sabgasim@gmail.com>
2023-08-22 14:42:43 +0300

    added SettingsContentView

---
+++ Social_Media_App_v2/NativeApp/MVVM/Views/ContentViews/SettingsContentView.xaml.cs
@@ -0,0 +1,67 @@
+
+using System.Windows.Input;
+
+namespace NativeApp.MVVM.Views.ContentViews;
+
+public partial class SettingsContentView : ContentView
+{
+	public SettingsContentView()
+	{
+		InitializeComponent();
+
+        this.BindingContext = this;
+	}
+
+    public static readonly BindableProperty TitleProperty = 
+        BindableProperty.Create(nameof(Title), typeof(string), typeof(SettingsContentView));
+
+    public static readonly BindableProperty DescriptionProperty =
+        BindableProperty.Create(nameof(Description), typeof(string), typeof(SettingsContentView));
+
+    public static readonly BindableProperty IconProperty =
+        BindableProperty.Create(nameof(Icon), typeof(ImageSource), typeof(SettingsContentView));
+
+    public static readonly BindableProperty CommandProperty =
+        BindableProperty.Create(nameof(Command), typeof(ICommand), typeof(SettingsContentView));
+
+    public static readonly BindableProperty CommandParameterProperty =
+        BindableProperty.Create(nameof(CommandParameter), typeof(object), typeof(SettingsContentView));
+
+    public string Title
+    {
+        get => (string)GetValue(TitleProperty);
+        set => SetValue(TitleProperty, value);
+    }
+
+    public string Description
+    {
+        get => (string)GetValue(DescriptionProperty);
+        set => SetValue(DescriptionProperty, value);
+    }
+
+    public ImageSource Icon
+    {
+        get => (ImageSource)GetValue(IconProperty);
+        set => SetValue(IconProperty, value);
+    }
+
+    public ICommand? Command
+    {
+        get => (ICommand)GetValue(CommandProperty);
+        set => SetValue(CommandProperty, value);
+    }
+
+    public object? CommandParameter
+    {
+        get => GetValue(CommandParameterProperty);
+        set => SetValue(CommandParameterProperty, value);
+    }
+
+    private void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
+    {
+        if (Command is not null && Command.CanExecute(CommandParameter))
+        {
+            Command.Execute(CommandParameter);
+        }
+    }
+}
