4ce5f4351d42beb58fa1b9e2d64e5c65dad34bd9
Sabig Gasim Bakry <sabgasim@gmail.com>
2023-08-28 22:53:53 +0300

    added ValidatableObject.cs

---
+++ Social_Media_App_v2/NativeApp/MVVM/ViewModels/ValidatableObject.cs
@@ -0,0 +1,43 @@
+﻿using NativeApp.Interfaces;
+
+namespace NativeApp.MVVM.ViewModels;
+public class ValidatableObject<T> : ViewModelBase
+{
+    private IEnumerable<string>? _errors;
+    private bool _isValid;
+    private T? _value;
+    public List<IValidationRule<T>>? Validations { get; } = new();
+    
+    public ValidatableObject()
+    {
+        _isValid = true;
+        _errors = Enumerable.Empty<string>();
+    }
+
+    public IEnumerable<string>? Errors
+    {
+        get => _errors;
+        private set => TrySetValue(ref _errors, value);
+    }
+    public bool IsValid
+    {
+        get => _isValid;
+        private set => TrySetValue(ref _isValid, value);
+    }
+    public T? Value
+    {
+        get => _value;
+        set => TrySetValue(ref _value, value);
+    }
+
+    public bool Validate()
+    {
+        Errors = Validations
+            ?.Where(v => !v.Check(Value))
+            ?.Select(v => v.ValidationMessage)
+            ?? Enumerable.Empty<string>();
+
+        IsValid = !Errors.Any();
+        return IsValid;
+    }
+}
