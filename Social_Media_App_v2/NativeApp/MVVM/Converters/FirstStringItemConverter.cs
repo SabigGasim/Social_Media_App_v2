82059e286d23cde334a7b9b027ed8b90ef7ea7d7
Sabig Gasim Bakry <sabgasim@gmail.com>
2023-08-28 22:57:07 +0300

    added converters to deal with validation errors

---
+++ Social_Media_App_v2/NativeApp/MVVM/Converters/FirstStringItemConverter.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+namespace NativeApp.MVVM.Converters;
+public class FirstStringItemConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if(value is IEnumerable<string> list)
+        {
+            return list.FirstOrDefault() ?? "";
+        }
+
+        return "";
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
