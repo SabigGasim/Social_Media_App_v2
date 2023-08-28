7a7114a57ffaae1dbe2365c5828581a2ddd94a0c
Sabig Gasim Bakry <sabgasim@gmail.com>
2023-08-28 23:35:26 +0300

    fix NullReferenceException at INavigationService.PopAsync

--- Social_Media_App_v2/NativeApp/Interfaces/INavigationSerivce.cs
+++ Social_Media_App_v2/NativeApp/Interfaces/INavigationSerivce.cs
@@ -3,8 +3,6 @@
 namespace NativeApp.Interfaces;
 public interface INavigationService
 {
-    Task InitializeAsync();
-
     Task NavigateToAsync(string route, IDictionary<string, object> routeParameters = null);
     Task<object?> ShowPopupAsync<TPopup>(TPopup popup) where TPopup : Popup;
 
--- Social_Media_App_v2/NativeApp/Services/INavigationService.cs
+++ Social_Media_App_v2/NativeApp/Services/INavigationService.cs
@@ -5,11 +5,6 @@ namespace NativeApp.Services;
 
 public class MauiNavigationService : INavigationService
 {
-    public Task InitializeAsync()
-    {
-        throw new NotImplementedException();
-    }
-
     public Task NavigateToAsync(string route, IDictionary<string, object> routeParameters = null) =>
         routeParameters is not null
             ? Shell.Current.GoToAsync(route, routeParameters)
