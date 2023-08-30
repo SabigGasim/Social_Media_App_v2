7458fbf6b4b053e8e330e740212e3a04c3c63bd3
Sabig Gasim Bakry <sabgasim@gmail.com>
2023-08-30 19:51:09 +0300

    added whitespace rule

---
+++ Social_Media_App_v2/NativeApp/Validations/RegularExpressions/WhiteSpaceRegex.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace NativeApp.Validations.RegularExpressions;
+public static partial class WhiteSpaceRegex
+{
+    private const string _whiteSpaceRegex = @"\s+$";
+
+    [GeneratedRegex(_whiteSpaceRegex)]
+    private static partial Regex WhiteSpaceRegexGenerator();
+
+    public static bool IsMatch(string input)
+    {
+        return WhiteSpaceRegexGenerator().IsMatch(input);
+    }
+}
