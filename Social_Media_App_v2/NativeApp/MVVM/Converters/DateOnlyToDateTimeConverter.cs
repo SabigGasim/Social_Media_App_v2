60fa7a21bc0afc089d936f3b2a91dcdc98aaf553
Sabig Gasim Bakry <sabgasim@gmail.com>
2023-08-30 21:51:48 +0300

    add DateOnlyToDateTimeConverter.cs

---
+++ Social_Media_App_v2/NativeApp/MVVM/Converters/DateOnlyToDateTimeConverter.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+
+namespace NativeApp.MVVM.Converters;
+public class DateOnlyToDateTimeConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if(value is DateOnly dateOnly)
+        {
+            return dateOnly.ToDateTime(TimeOnly.MinValue);
+        }
+
+        return default(DateTime);
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is DateTime dateTime)
+        {
+            return DateOnly.FromDateTime(dateTime);
+        }
+
+        return default(DateOnly);
+    }
+}
