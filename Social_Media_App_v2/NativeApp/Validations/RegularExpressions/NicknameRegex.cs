291435b5d40f6d1d1cce35eb5968ac6a827ef5cb
Sabig Gasim Bakry <sabgasim@gmail.com>
2023-08-30 19:50:17 +0300

    added nickname rule

---
+++ Social_Media_App_v2/NativeApp/Validations/RegularExpressions/NicknameRegex.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace NativeApp.Validations.RegularExpressions;
+public partial class NicknameRegex
+{
+    private const string _nicknameRegex = @"^[a-zA-Z0-9_.\s*]+$";
+
+    [GeneratedRegex(_nicknameRegex, RegexOptions.IgnoreCase)]
+    private static partial Regex NicknameRegexGenerator();
+
+    public static bool IsMatch(string input)
+    {
+        return NicknameRegexGenerator().IsMatch(input);
+    }
+}
+
