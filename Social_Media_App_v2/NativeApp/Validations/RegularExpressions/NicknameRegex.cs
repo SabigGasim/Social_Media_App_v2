08f27a3fae3d8befbd6cdb2a0f5564e66f5dfcb6
Sabig Gasim Bakry <sabgasim@gmail.com>
2023-09-01 16:15:10 +0300

    add Hindi alphabet support in NicknameRegex

--- Social_Media_App_v2/NativeApp/Validations/RegularExpressions/NicknameRegex.cs
+++ Social_Media_App_v2/NativeApp/Validations/RegularExpressions/NicknameRegex.cs
@@ -1,16 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
+﻿using System.Text.RegularExpressions;
 
 namespace NativeApp.Validations.RegularExpressions;
+
+/// <summary>
+/// This regex is only responsible of matching alphabetical letters and numbers from many different languages.
+/// It's not resposible of checking empty strings or white spaces.
+/// </summary>
 public partial class NicknameRegex
 {
-    private const string _nicknameRegex = @"^[a-zA-Z0-9_.\s*]+$";
+    private const string _nicknameRegex = @"^[\p{L}\p{N}_.\s\p{IsDevanagari}]*$";
 
-    [GeneratedRegex(_nicknameRegex, RegexOptions.IgnoreCase)]
+    [GeneratedRegex(_nicknameRegex, RegexOptions.ECMAScript | RegexOptions.IgnoreCase)]
     private static partial Regex NicknameRegexGenerator();
 
     public static bool IsMatch(string input)
