4d15950a420efd64c28a42168b16706cf617e1b3
Sabig Gasim Bakry <sabgasim@gmail.com>
2023-09-08 01:25:30 +0300

    attatch MutedAndBlockedUsersViewModelBase changes

--- Social_Media_App_v2/NativeApp/MVVM/ViewModels/Settings/MutedAndBlocked/MutedAndBlockedUsersViewModelBase.cs
+++ Social_Media_App_v2/NativeApp/MVVM/ViewModels/Settings/MutedAndBlocked/MutedAndBlockedUsersViewModelBase.cs
@@ -4,22 +4,16 @@ using System.ComponentModel;
 
 namespace NativeApp.MVVM.ViewModels.Settings.MutedAndBlocked;
 
-public abstract class MutedAndBlockedUsersViewModelBase<TViewModel> : ViewModelBase
+public abstract class MutedAndBlockedUsersViewModelBase : ViewModelBase
 {
-    private readonly Func<RangeObservableCollection<UserModel>?> _usersGetter;
-    private readonly Action<RangeObservableCollection<UserModel>?> _usersSetter;
     private readonly Subscriber _mutedUsersSubscriber;
     private readonly IServiceProvider _serviceProvider;
     private readonly PropertyChangedEventHandler OnMutedUsersChanged;
 
     public MutedAndBlockedUsersViewModelBase(
         MutedAndBlockedViewModel viewModel,
-        IServiceProvider serviceProvider,
-        Func<RangeObservableCollection<UserModel>?> usersGetter,
-        Action<RangeObservableCollection<UserModel>?> usersSetter)
+        IServiceProvider serviceProvider)
     {
-        _usersGetter = usersGetter;
-        _usersSetter = usersSetter;
         _mutedUsersSubscriber = viewModel.Subscriber;
         _serviceProvider = serviceProvider;
 
@@ -36,13 +30,16 @@ public abstract class MutedAndBlockedUsersViewModelBase<TViewModel> : ViewModelB
 
     public RangeObservableCollection<UserViewModel>? UserViewModels
     {
-        get => MapToUserViewModels(_usersGetter());
+        get => MapToUserViewModels(UsersGetter());
         set
         {
-            _usersSetter(new(value?.Select(x => x.User)!));
+            UsersSetter(new(value?.Select(x => x.User)!));
             OnPropertyChanged(nameof(UserViewModels));
         }
     }
+
+    protected abstract Func<RangeObservableCollection<UserModel>?> UsersGetter { get; init; }
+    protected abstract Action<RangeObservableCollection<UserModel>?> UsersSetter { get; init; }
 
     private RangeObservableCollection<UserViewModel>? MapToUserViewModels(IEnumerable<UserModel>? userModels)
     {
