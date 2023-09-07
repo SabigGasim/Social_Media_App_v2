f6a5ad951ed8e80a08fe34ae73f5933fe136db52
Sabig Gasim Bakry <sabgasim@gmail.com>
2023-09-08 01:29:41 +0300

    added BlockedAccountsViewModel.cs

---
+++ Social_Media_App_v2/NativeApp/MVVM/ViewModels/Settings/MutedAndBlocked/BlockedAccountsViewModel.cs
@@ -0,0 +1,18 @@
+﻿using NativeApp.Helpers;
+using NativeApp.MVVM.Models;
+
+namespace NativeApp.MVVM.ViewModels.Settings.MutedAndBlocked;
+
+public class BlockedAccountsViewModel : MutedAndBlockedUsersViewModelBase
+{
+    public BlockedAccountsViewModel(
+        MutedAndBlockedViewModel viewModel, 
+        IServiceProvider serviceProvider) : base(viewModel, serviceProvider)
+    {
+        UsersGetter = () => viewModel.Model?.BlockedUsers;
+        UsersSetter = (value) => viewModel.Model!.BlockedUsers = value;
+    }
+
+    protected override Func<RangeObservableCollection<UserModel>?> UsersGetter { get; init; }
+    protected override Action<RangeObservableCollection<UserModel>?> UsersSetter { get; init; }
+}
