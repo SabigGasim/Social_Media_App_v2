dfe64c0b5ad8359e940ccb77df45b591c227e117
Sabig Gasim Bakry <sabgasim@gmail.com>
2023-09-10 01:14:55 +0300

    added DurationToStringConverter

---
+++ Social_Media_App_v2/NativeApp/MVVM/Converters/DurationToStringConverter.cs
@@ -0,0 +1,46 @@
+﻿using Domain.Enums;
+using Humanizer;
+using System.Globalization;
+
+namespace NativeApp.MVVM.Converters;
+public class DurationToStringConverter : IValueConverter
+{
+    private readonly Dictionary<Duration, string> _convertCache = new();
+    private readonly Dictionary<string, Duration> _convertBackCache = new();
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if(value is Duration duration)
+        {
+            if(_convertCache.TryGetValue(duration, out string? result))
+            {
+                return result;
+            }
+
+            var name = duration.Humanize(LetterCasing.Sentence);
+            _convertCache.Add(duration, name);
+            
+            return name;
+        }
+
+        return "";
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if(value is string str)
+        {
+            if (_convertBackCache.TryGetValue(str, out Duration result))
+            {
+                return result;
+            }
+
+            var duration = Enum.Parse<Duration>(str.Dehumanize());
+            _convertBackCache.Add(str, result);
+
+            return duration;
+        }
+
+        return Duration.Unspecified;
+    }
+}
