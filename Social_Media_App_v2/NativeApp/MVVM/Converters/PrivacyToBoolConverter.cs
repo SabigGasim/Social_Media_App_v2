37c748b4a5658e274903920c2b4d4e5c39d750b7
Sabig Gasim Bakry <sabgasim@gmail.com>
2023-09-10 18:23:22 +0300

    added PrivacyToBoolConverter

---
+++ Social_Media_App_v2/NativeApp/MVVM/Converters/PrivacyToBoolConverter.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Enums;
+using System.Globalization;
+
+namespace NativeApp.MVVM.Converters;
+public class PrivacyToBoolConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if(value is Privacy privacy)
+        {
+            return privacy == Privacy.Private;
+        }
+
+        return false;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if(value is bool isPrivate)
+        {
+            return isPrivate ? Privacy.Private : Privacy.Public;
+        }
+
+        return default(Privacy);
+    }
+}
